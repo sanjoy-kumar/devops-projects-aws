48dc1e73af30b5fd9684deb8ff90eeb2e3797e8a
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2024-12-15 20:26:02 -0500

    Add 4 subnets(2 public + 2 private)

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -22,9 +22,52 @@ provider "aws" {
 # enable_dns_hostnames = true
 
 
-resource "aws_vpc" "devops-aws" {
+resource "aws_vpc" "devops_aws" {
   cidr_block = "10.15.0.0/23"
   tags = {
     Name = "devops-aws"
   }
 }
+
+
+# Create public and private subnets: 4 subnets(2 public + 2 private)
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.devops_aws.id
+  cidr_block              = "10.15.0.0/25"
+  map_public_ip_on_launch = true
+  availability_zone       = "ca-central-1a"
+  tags = {
+    Name = "PublicSubnet1-devops-aws"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.devops_aws.id
+  cidr_block        = "10.15.0.128/25"
+  availability_zone = "ca-central-1a"
+  tags = {
+    Name = "PrivateSubnet1-devops-aws"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.devops_aws.id
+  cidr_block              = "10.15.1.0/25"
+  map_public_ip_on_launch = true
+  availability_zone       = "ca-central-1b"
+  tags = {
+    Name = "PublicSubnet2-devops-aws"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.devops_aws.id
+  cidr_block        = "10.15.1.128/25"
+  availability_zone = "ca-central-1b"
+  tags = {
+    Name = "PrivateSubnet2-devops-aws"
+  }
+}
+
+
