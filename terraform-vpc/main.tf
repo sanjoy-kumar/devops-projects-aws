deb923aa06259bb5f44d72aee29433c11db51ab1
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2024-12-15 21:18:30 -0500

    Add Security Groups

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -155,8 +155,41 @@ resource "aws_route_table_association" "private_subnet_association_2" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
-# -------------------  
+# -------------------  8.0 Add Security Groups (Optional) --------------------------------
+## Define a security group allowing SSH, HTTP, and HTTPS access:
 
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.devops_aws.id
+  tags = {
+    Name = "PublicSecurityGroup-devops-aws"
+  }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
