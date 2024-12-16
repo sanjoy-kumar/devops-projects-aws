1c4664329f6a593329a1cf2c59b115f93a1cd9eb
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2024-12-15 20:35:00 -0500

    Add a Route Table for Public Subnets

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -71,3 +71,42 @@ resource "aws_subnet" "private_subnet_2" {
 }
 
 
+# Define an Internet Gateway:
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.devops_aws.id
+  tags = {
+    Name = "InternetGateway-devops-aws"
+  }
+}
+
+
+# Create a Route Table for Public Subnets
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.devops_aws.id
+  tags = {
+    Name = "PublicRouteTable-devops-aws"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+
+
+
